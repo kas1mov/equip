5ec9848f074ed8b9622430a01ec32f36823ef085
kas1mov <jaloladdinkosimov@gmail.com>
2023-05-15 20:35:14 +0500

    /change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import { Footer } from "./companents/Footer/footer";
-import { Header } from "./companents/Header/header";
-import { Home } from "./pages/home";
+import { Root } from "./companents/root";
 
 function App() {
   return (
     <div className="App">
-      <div className="Header">
-        <Header />
-      </div>
-      <div className="Home">
-        <Home />
-      </div>
-      <div className="Footer">
-        <Footer />
-      </div>
+      <Root />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ import { Buy } from "./pages/buy";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/buy" element={<Buy />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/buy" element={<Buy />} />
+    </Routes>
+  </BrowserRouter>
 );
